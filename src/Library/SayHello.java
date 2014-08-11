575ea9be443fff777c8f99a508b7332d342421c3
hl <l0huan08@gmail.com>
2014-08-11 17:58:46 -0400

    create first file

---
+++ src/Library/SayHello.java
@@ -0,0 +1,13 @@
+package Library;
+
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heeeloo);");
+	}
+
+}
