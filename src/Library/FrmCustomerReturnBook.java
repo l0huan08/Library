0cc7e6d8890526d7b6322a484dbc4d71c8d8274f
runyan <yanrun2007@gmail.com>
2014-08-12 14:27:23 -0400

    Customer Return Book

---
+++ src/Library/FrmCustomerReturnBook.java
@@ -0,0 +1,45 @@
+package Library;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class FrmCustomerReturnBook extends JFrame{
+	private JFrame jf;
+	private JLabel msg;
+	private JButton Return, close;
+	private JTable books;
+	FrmCustomerReturnBook(){
+		jf = new JFrame();
+		jf.setLayout(null);
+		msg = new JLabel();
+		msg.setText("Rented Books");
+		msg.setBounds(20, 20, 80, 10);
+		Return = new JButton("Return");
+		Return.setBounds(60, 550, 75, 20);
+		close = new JButton("Close");
+		close.setBounds(180, 550, 75, 20);
+		books = new JTable();
+		books.setBounds(20, 35, 300, 500);
+		jf.add(msg);
+		jf.add(books);
+		jf.add(Return);
+		jf.add(close);
+		jf.setTitle("Return Book");
+		jf.setSize(650, 650);
+		jf.setResizable(false);
+		jf.setVisible(true);
+		close.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent me){
+				jf.setVisible(false);
+				new FrmCustomerLogin();
+			}
+		});
+	}
+	public static void main(String[] args){
+		new FrmCustomerReturnBook();
+	}
+}
