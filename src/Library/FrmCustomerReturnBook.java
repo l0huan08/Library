c5a97d37dc0741dd3f62d0cfa48c65a59e780afb
runyan <yanrun2007@gmail.com>
2014-08-13 10:54:36 -0400

    return book

--- src/Library/FrmCustomerReturnBook.java
+++ src/Library/FrmCustomerReturnBook.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 /**
  * @author Run Yan
@@ -15,7 +15,10 @@ import java.awt.event.*;
 
 @SuppressWarnings("serial")
 public class FrmCustomerReturnBook extends JFrame{
+	private final int N_Book_Table_Columns = 2;
 	private final String[] tbBookColumnTitle = {"isbn", "name"};
+	private Library library;
+	private User user;
 	private JFrame frmJf;
 	private JLabel lblMsg;
 	private JButton btnReturn, btnClose;
@@ -24,6 +27,11 @@ public class FrmCustomerReturnBook extends JFrame{
 	private PanelBookInfo pnlBookInfo;
 	private DefaultTableModel tbBooksModel;
 	
+	FrmCustomerReturnBook(Library l, User u){
+		this();
+		this.library = l;
+		this.user = u;
+	}
 	FrmCustomerReturnBook(){
 		frmJf = new JFrame();
 		frmJf.setLayout(null);
@@ -39,7 +47,7 @@ public class FrmCustomerReturnBook extends JFrame{
 		btnReturn.setBounds(80, 400, 75, 30);
 		btnClose = new JButton("Close");
 		btnClose.setBounds(220, 400, 75, 30);
-		tblBooks = new JTable();
+		tblBooks = new JTable(null,tbBookColumnTitle);
 		tblBooks.setBorder(new LineBorder(new Color(0, 0, 0)));
 		tblBooks.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		tbBooksModel = new DefaultTableModel(
@@ -48,7 +56,7 @@ public class FrmCustomerReturnBook extends JFrame{
 				tbBookColumnTitle
 		);
 		tblBooks.setModel(tbBooksModel);
-		tblBooks.setBounds(20, 35, 1000, 330);
+		tblBooks.setBounds(20, 35, 380, 330);
 		pnlLeft.add(lblMsg);
 		pnlLeft.add(tblBooks);
 		pnlLeft.add(btnReturn);
@@ -65,6 +73,14 @@ public class FrmCustomerReturnBook extends JFrame{
 		frmJf.setSize(900, 500);
 		frmJf.setResizable(false);
 		frmJf.setVisible(true);
+		btnReturn.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent me){
+				//new DlgCustomerReturnBookFinish();
+				double fine = 20;
+				new DlgCustomerReturnBookFinish(fine);
+			}
+		});
 		btnClose.addMouseListener(new MouseAdapter()
 		{
 			public void mouseClicked(MouseEvent me){
@@ -72,8 +88,45 @@ public class FrmCustomerReturnBook extends JFrame{
 				new FrmCustomerLogin();
 			}
 		});
+		
+	}
+	private void refreshTable(){
+		int n = tbBooksModel.getRowCount();
+		for(int i = 0;i < n;i++){
+			tbBooksModel.removeRow(0);
+		}
+		Object[][] data = getRentedBookTableData();
+		this.tbBooksModel.addRow(data);;
+	}
+	private void returnBook(){
+		int columns = tblBooks.getColumnCount();
+		
+	}
+	private Object[] createBookTableRowData(Book book){
+		Object[] row = new Object[N_Book_Table_Columns];
+		row[0] = book.getIsbn();
+		row[1] = book.getBookName();
+		return row;
+	}
+	private Object[][] getRentedBookTableData(){
+		if(this.library == null){
+			return null;
+		} else{
+			if(this.user == null){
+				return null;
+			} else{
+				ArrayList<Book> books = library.showBookList_BorrowedByCustomer(user.getUserId());
+				int nBooks = books.size();
+				Object[][] booksData = new Object[nBooks][];
+				for(int i = 0;i < nBooks;i++){
+					booksData[i] = createBookTableRowData(books.get(i));
+				}
+				return booksData;
+			}
+		}
 	}
 	public static void main(String[] args){
-		new FrmCustomerReturnBook();
+		FrmCustomerReturnBook Return = new FrmCustomerReturnBook();
+		//Return.tblBooks = new JTable(null, tbBookColumnTitle);
 	}
 }
