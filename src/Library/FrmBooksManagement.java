4c4b0ca5827c4e657a87bf2d9edb09eb36840d90
Zhipeng-Zhou <roczhouzhipeng@gmail.com>
2014-08-12 20:13:44 -0400

    Book Management Frame. Includes first "table" of listing current "books" Block for showing images + info. of these books still expected from Li Huang

--- src/Library/FrmBooksManagement.java
+++ src/Library/FrmBooksManagement.java
@@ -1,16 +1,62 @@
 package Library;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*; //javax.swing.JFrame;
+
+import com.sun.prism.paint.Color;
+
+import java.io.*;
+import java.util.Date;
 
 public class FrmBooksManagement {
 	public FrmBooksManagement(){
 		JFrame L = new JFrame();
 		L.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		L.setSize(800, 600);
+		L.setSize(1000, 800);
 		L.setLocation(250,40);
 		L.setVisible(true);
-		L.setTitle("Admin Login");		
+		L.setTitle("Books Management");	
 		L.setLayout(null);
+
+	    Object rowData[][] = { { "GBF", "SomeOne", "0032MN","", "Row1-Column1", "Row1-Column2", "Row1-Column3","" },
+	    						{ "Row1-Column1", "Row1-Column2", "Row1-Column3","", "Row1-Column1", "Row1-Column2", "Row1-Column3","" },
+	    };
+	    
+	    Object columnNames[] = { "bookName", "author", "isbn", "lastRented",
+				"AddedDate", "isRented", "ownerId", "category" };
+	        
+	    JTable table = new JTable(rowData, columnNames);
+
+	    JScrollPane scrollPane = new JScrollPane(table);
+	    scrollPane.setBounds(50, 50, 600, 400);
+	    L.add(scrollPane);
+	    
+		JButton BtnAdd = new JButton("Add");
+		BtnAdd.setVisible(true);
+		BtnAdd.setBounds(110, 520, 120, 30);
+		L.add(BtnAdd);
 		
+		JButton BtnUpdate = new JButton("Update");
+		BtnUpdate.setVisible(true);
+		BtnUpdate.setBounds(250, 520, 120, 30);
+		L.add(BtnUpdate);
+		
+		JButton BtnDelete = new JButton("Delete");
+		BtnDelete.setVisible(true);
+		BtnDelete.setBounds(390, 520, 120, 30);
+		L.add(BtnDelete);
+		
+		JButton BtnReport = new JButton("RentedBook Report");
+		BtnReport.setVisible(true);
+		BtnReport.setBounds(760, 520, 150, 30);
+		L.add(BtnReport);
+		
+		JButton BtnExit = new JButton("Exit");
+		BtnExit.setVisible(true);
+		BtnExit.setBounds(785, 560, 100, 30);
+		L.add(BtnExit);
+
 	}
 }
