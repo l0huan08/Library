3dad2849d821094b1da74e7fa49680c5b4b12b22
runyan <yanrun2007@gmail.com>
2014-08-14 17:28:59 -0400

    about frame

---
+++ src/Library/FrmAbout.java
@@ -0,0 +1,96 @@
+package Library;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
+import java.util.*;
+import javax.swing.*;
+
+
+/**
+ * About form, show author and version information
+ * @author Run Yan
+ * add 2014.8.14
+ */
+public class FrmAbout extends JDialog{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField txtAuthor;
+	private JTextField txtVersion;
+	private JButton btnOk;
+	
+	private Random r;
+	
+	private final String StrAuthors = "Zhipeng Zhou, Run Yan, Sen Li, Li Huang";
+	private final String StrVersion = "version: "+ new DecimalFormat("#.00").format(Math.PI);
+	
+	public FrmAbout() {
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setTitle("About");
+		this.setSize(500,500);
+		this.setLayout(null);
+		
+		r = new Random();
+		
+		txtAuthor = new JTextField();
+		txtAuthor.setBounds(50,50,400,50);
+		txtAuthor.setBorder(null);
+		txtAuthor.setBackground(this.getBackground());
+		txtAuthor.setFont(new Font(null, Font.BOLD, 20));
+		
+		txtVersion = new JTextField();
+		txtVersion.setBounds(195,180,130,30);
+		txtVersion.setBorder(null);
+		txtVersion.setBackground(this.getBackground());
+		txtVersion.setFont(new Font(null, Font.BOLD, 20));
+		
+		txtAuthor.setAutoscrolls(true);
+		
+		btnOk = new JButton("OK");
+		btnOk.setBounds(215,320,70,50);
+		
+		this.add(txtAuthor);
+		this.add(txtVersion);
+		this.add(btnOk);
+		
+		//--------- init -----------------
+		txtAuthor.setText(StrAuthors);
+		txtVersion.setText(StrVersion);
+		
+		
+		this.setVisible(true);
+		
+		txtAuthor.addMouseListener(new MouseAdapter()
+		{
+			public void mouseEntered(MouseEvent me){
+				int x = r.nextInt(400);
+				int y = r.nextInt(400);
+				txtAuthor.setLocation(x, y);
+			}
+		});
+		txtVersion.addMouseListener(new MouseAdapter()
+		{
+			public void mouseEntered(MouseEvent me){
+				int x = r.nextInt(400);
+				int y = r.nextInt(400);
+				txtVersion.setLocation(x, y);
+			}
+		});
+		btnOk.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae){
+				FrmAbout.this.dispose();
+			}
+		});
+	}
+	
+	public static void main(String args[]) {
+		new FrmAbout();
+	}
+}
