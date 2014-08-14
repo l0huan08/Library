1b62082bcdd00da020664f96149eb635dad1fdfb
hl <l0huan08@gmail.com>
2014-08-14 13:14:26 -0400

    edit Validator: add isUserValid(), isBookValid()

--- src/Library/Validator.java
+++ src/Library/Validator.java
@@ -1,5 +1,11 @@
 package Library;
 
+/**
+ * The class to validate User and Book
+ * @author Sen Li
+ * add 2014.8.13
+ * edit by Li Huang 2014.8.14: add isUserValid(), isBookValid()
+ */
 public class Validator {
 
 	/**
@@ -9,7 +15,7 @@ public class Validator {
 	 * @param str
 	 * @return
 	 */
-	public boolean isIsbnValid(String str) {
+	public boolean isBookIsbnValid(String str) {
 		final String invalidChars = "[ @#$%^&*=+~`|\\/?.,\';:\"]";
 
 		if (str == null)
@@ -27,6 +33,29 @@ public class Validator {
 		return true;
 	}
 
+	public boolean isBookNameValid(String str) {
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		return true;
+	}
+	
+	public boolean isBookAuthorValid(String str) {
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		return true;
+	}
+	
+	
 	public boolean isContentValid(String str) {
 		if (str == null)
 			return false;
@@ -103,6 +132,31 @@ public class Validator {
 		return true;
 	}
 
+	/**
+	 * whether the user is valid. used in Adding user.
+	 * @return
+	 */
+	public boolean isUserValid(User user) {
+		if (user==null)
+			return false;
+		
+		boolean bId = isUserIdValid(String.valueOf(user.getUserId()));
+		boolean bName = isUserNameValid(user.getUserName());
+		boolean bPassword = isUserPasswordValid(user.getPassword());
+		boolean bPhoneNo = isUserPhoneNoValid(user.getPassword());
+		return (bId && bName && bPassword && bPhoneNo);
+	}
+	
+	public boolean isBookValid(Book book) {
+		if (book==null)
+			return false;
+		
+		boolean bIsbn = isBookIsbnValid(book.getIsbn());
+		boolean bName = isBookNameValid(book.getBookName());
+		boolean bAuthor = isBookAuthorValid(book.getAuthor());
+		return (bIsbn && bName && bAuthor );
+	}
+	
 	/*
 	 * static public void main(String[] args) { Test t = new Test();
 	 * 
