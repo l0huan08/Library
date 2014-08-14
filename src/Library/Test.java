131e715310611d9cf5a058b08d8187c1c8bbb359
Sen Li <lisen827@gmail.com>
2014-08-14 11:40:42 -0600

    test methods updated

--- src/Library/Test.java
+++ src/Library/Test.java
@@ -3,49 +3,110 @@ package Library;
 public class Test {
 
 	/**
-	 * If str is null, or str=" ", or str="", or str contains "@#$%^&*-_=+~`|\/?.,';:\"" it is not vailid;
+	 * If str is null, or str=" ", or str="", or str contains
+	 * "@#$%^&*-_=+~`|\/?.,';:\"" it is not vailid;
+	 * 
 	 * @param str
 	 * @return
 	 */
 	public boolean isIsbnValid(String str) {
 		final String invalidChars = "[ @#$%^&*=+~`|\\/?.,\';:\"]";
-		
-		if (str==null)
+
+		if (str == null)
 			return false;
-		
+
 		str = str.trim();
-		if (str.length()==0)
+		if (str.length() == 0)
 			return false;
-		
-		str=str.replaceAll(invalidChars, ":");
-		
+
+		str = str.replaceAll(invalidChars, ":");
+
 		if (str.contains(":"))
 			return false;
-		
+
 		return true;
 	}
-	
+
 	public boolean isContentValid(String str) {
-		if (str==null)
+		if (str == null)
 			return false;
-		
+
 		str = str.trim();
-		if (str.length()==0)
+		if (str.length() == 0)
 			return false;
-		
+
 		return true;
 	}
-	
-	
-	
-	
-	
-	/*
-	static public void main(String[] args) {
-		Test t = new Test();
-		
-		System.out.println(		t.isIsbnValid("ha*ha") );
-		System.out.println(		t.isIsbnValid("haha12332-3232") );
+
+	public boolean isUserIdValid(String str) {
+
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		try {
+			@SuppressWarnings("unused")
+			int checkInt = Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+
 	}
-	*/
+
+	public boolean isUserNameValid(String str) {
+		final String invalidChars = "[ @#$%^&*=+~`|\\/?.,\';:\"]";
+
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		str = str.replaceAll(invalidChars, ":");
+
+		if (str.contains(":"))
+			return false;
+
+		return true;
+	}
+
+	public boolean isUserPasswordValid(String str) {
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		return true;
+	}
+
+	public boolean isUserPhoneNoValid(String str) {
+		if (str == null)
+			return false;
+
+		str = str.trim();
+		if (str.length() == 0)
+			return false;
+
+		try {
+			@SuppressWarnings("unused")
+			int checkInt = Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * static public void main(String[] args) { Test t = new Test();
+	 * 
+	 * System.out.println( t.isIsbnValid("ha*ha") ); System.out.println(
+	 * t.isIsbnValid("haha12332-3232") ); }
+	 */
 }
