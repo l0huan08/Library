eb9171cc9e4b44039e7cb95e2c2e0a840157abd5
Zhipeng-Zhou <roczhouzhipeng@gmail.com>
2014-08-14 14:44:42 -0400

    123

--- src/Library/FrmBooksManagement.java
+++ src/Library/FrmBooksManagement.java
@@ -67,6 +67,50 @@ public class FrmBooksManagement extends JFrame {
 		btnClose.setBounds(880, 620, 100, 30);
 		this.add(btnClose);
 		
+		JLabel lStatus = new JLabel();
+		lStatus.setVisible(true);
+		lStatus.setText("Status:");
+		lStatus.setBounds(705, 103, 50, 25);
+		this.add(lStatus);
+		
+		JTextField tStatus = new JTextField();
+		tStatus.setVisible(true);
+		tStatus.setBounds(750, 100, 200, 25);
+		this.add(tStatus);
+		
+		JLabel lISBN = new JLabel();
+		lISBN.setVisible(true);
+		lISBN.setText("ISBN:");
+		lISBN.setBounds(709, 143, 50, 25);
+		this.add(lISBN);
+		
+		JTextField tISBN = new JTextField();
+		tISBN.setVisible(true);
+		tISBN.setBounds(750, 140, 200, 25);
+		this.add(tISBN);
+		
+		JLabel lName = new JLabel();
+		lName.setVisible(true);
+		lName.setText("Book Name:");
+		lName.setBounds(675, 182, 70, 30);
+		this.add(lName);
+		
+		JTextField tName = new JTextField();
+		tName.setVisible(true);
+		tName.setBounds(750, 180, 200, 25);
+		this.add(tName);
+		
+		JLabel lAuthor = new JLabel();
+		lAuthor.setVisible(true);
+		lAuthor.setText("Author Name:");
+		lAuthor.setBounds(665, 222, 80, 25);
+		this.add(lAuthor);
+		
+		JTextField tAuthor = new JTextField();
+		tAuthor.setVisible(true);
+		tAuthor.setBounds(750, 220, 200, 25);
+		this.add(tAuthor);
+		
 		btnReport.addActionListener(new java.awt.event.ActionListener() {
 		    public void actionPerformed(java.awt.event.ActionEvent evt) {
 		    	FrmBookReport N = new FrmBookReport();		    	
@@ -86,20 +130,93 @@ public class FrmBooksManagement extends JFrame {
 		    }
 		});
 		
-		JButton btnRefresh = new JButton("Refresh");
-		btnRefresh.setVisible(true);
-		btnClose.setBounds(880, 620, 100, 30);
-		this.add(btnClose);
-		
-		btnRefresh.addActionListener(new java.awt.event.ActionListener() {
+		btnDelete.addActionListener(new java.awt.event.ActionListener() {
 		    public void actionPerformed(java.awt.event.ActionEvent evt) {
-	    	FrmAddBooks N = new FrmAddBooks(L); 
+			      lStatus.setVisible(false);
+			      tStatus.setVisible(false);
+			      lISBN.setVisible(false);
+			      tISBN.setVisible(false);
+				  lName.setVisible(false);
+				  tName.setVisible(false);
+				  lAuthor.setVisible(false);
+				  tAuthor.setVisible(false);
+				  JLabel lDelete = new JLabel();
+				  lDelete.setVisible(true);
+				  lDelete.setText("Click the book you want to delete");
+				  lDelete.setBounds(665, 222, 300, 25);
+				  FrmBooksManagement.this.add(lDelete);
+				  
+				table.addMouseListener(new MouseAdapter() {
+				  public void mouseClicked(MouseEvent e) {
+				      JTable target = (JTable)e.getSource();
+				      
+				      int row = target.getSelectedRow();   
+				      int column = target.getSelectedColumn();	// useless here
+				      dtm.removeRow(row);
+				      L.deleteBook(table.getValueAt(row, 2).toString());
+	
+				  }
+				});
 		    }
 		});
-
-		}
-	
 		
+		btnUpdate.addActionListener(new java.awt.event.ActionListener() {
+    public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+				  
+				table.addMouseListener(new MouseAdapter() {
+				  public void mouseClicked(MouseEvent e) {
+				      JTable target = (JTable)e.getSource();
+				      
+				      int row = target.getSelectedRow();  
+				      Book tempBook = new Book();
+				     //set new information:
+//				      tempBook.
+				    
+				  }
+				});
+		    }
+		});
+		
+		JButton btnRefresh = new JButton("Refresh");
+		btnRefresh.setVisible(true);
+		btnRefresh.setBounds(250, 470, 120, 30);
+		this.add(btnRefresh);
+		btnRefresh.addActionListener(new java.awt.event.ActionListener() {
+		    public void actionPerformed(java.awt.event.ActionEvent evt) {
+		    	int j = table.getRowCount();
+		    	for (int i = 0 ; i < j; i++){
+			    	dtm.removeRow(0);			    	
+		    	}
+		    	for (int i = 0 ; i < L.showBookList_all().size(); i++){
+		    		dtm.addRow(createBookTableRowData(L.showBookList_all().get(i)));
+		    	}
+		    }
+		});
+		
+		table.addMouseListener(new MouseAdapter() {
+			  public void mouseClicked(MouseEvent e) {
+			      JTable target = (JTable)e.getSource();
+			      int row = target.getSelectedRow();   
+			      int column = target.getSelectedColumn();	// useless for here
+			      // Status
+			      if(table.getValueAt(row, 6) == "0"){
+			    	  tStatus.setText("Not Rented");
+			      }else{ tStatus.setText("Rented"); }
+			      
+			      // ISBN (ID Number)
+			      tISBN.setText(table.getValueAt(row, 2).toString());
+			      
+			      // Book Name
+			      tName.setText(table.getValueAt(row, 0).toString());
+			      
+			      // Book Author
+			      tAuthor.setText(table.getValueAt(row, 1).toString());
+
+			  }
+			});
+
+		}		
 	
 	private Object[][] getAllBookTableData(Library library) {
 		if (library==null)
@@ -130,6 +247,7 @@ public class FrmBooksManagement extends JFrame {
 		
 		return row;
 	}
+	
 	public FrmBooksManagement(){
 		
 	}
