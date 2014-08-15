23c24ceb17e8ce9fe753776631fa097ea005a867
hl <l0huan08@gmail.com>
2014-08-15 00:06:07 -0400

    fix bugs.

--- src/Library/FrmViewCategories.java
+++ src/Library/FrmViewCategories.java
@@ -1,20 +1,52 @@
 package Library;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-//Joe Simply a Frame that lists all the Categories we have for different books
+//Joe
 
-public class FrmViewCategories {
-	JFrame L = new JFrame("View Categories");
+/**
+ * Simply a Frame that lists all the Categories we have for different books
+ * @author Joe
+ * add 2014.8.11
+ * edit by Li Huang 2014.8.14 show all the categories
+ */
+public class FrmViewCategories extends JFrame {
+	//JFrame L = new JFrame("View Categories");
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final String StrCategories = "Children\nCooking\nHistory\nTravel";
+	
 	public FrmViewCategories(){
-		L.setSize(300, 400);
-		L.setLocation(250,40);
-		L.setVisible(true);
-		L.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.setTitle("Book Categories");
+		this.setSize(300, 450);
+		this.setLocation(250,40);
+		this.setVisible(true);
+		this.setLayout(null);
 
 		JTextArea TF = new JTextArea();
-		TF.setVisible(true);
 		TF.setBounds(20, 50, 260, 300);
-		L.add(TF);
+		TF.setEditable(false);
+		TF.setText(StrCategories);
+		this.add(TF);
+		
+		JButton btnOk = new JButton("Close");
+		btnOk.setBounds(120, 360, 70, 40);
+		btnOk.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+			
+		});
+		this.add(btnOk);
 	}
 }
